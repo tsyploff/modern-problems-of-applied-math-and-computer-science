6bcdb8673c32181f434cc70f4334b2532e54016f
Алексей <tsyplov80@mail.ru>
2021-10-24 18:53:44 +0300

    black_formatting

--- system_dynamic/dsolvemethods/runge_kutta.py
+++ system_dynamic/dsolvemethods/runge_kutta.py
@@ -9,14 +9,22 @@ class RungeKutta:
     def __str__(self):
         return f"RungeKutta(h={self.h})"
 
-    def solve(self, cauchy_problem: CauchyProblem, period: float) -> Tuple[List[float], List[float]]:
+    def solve(
+        self, cauchy_problem: CauchyProblem, period: float
+    ) -> Tuple[List[float], List[float]]:
         args = [cauchy_problem.arg]
         values = [cauchy_problem.value]
         for _ in range(round(period / self.h)):
             k_1 = cauchy_problem.derivative(args[-1], values[-1])
-            k_2 = cauchy_problem.derivative(args[-1] + self.h / 2, values[-1] + self.h * k_1 / 2)
-            k_3 = cauchy_problem.derivative(args[-1] + self.h / 2, values[-1] + self.h * k_2 / 2)
-            k_4 = cauchy_problem.derivative(args[-1] + self.h, values[-1] + self.h * k_3)
+            k_2 = cauchy_problem.derivative(
+                args[-1] + self.h / 2, values[-1] + self.h * k_1 / 2
+            )
+            k_3 = cauchy_problem.derivative(
+                args[-1] + self.h / 2, values[-1] + self.h * k_2 / 2
+            )
+            k_4 = cauchy_problem.derivative(
+                args[-1] + self.h, values[-1] + self.h * k_3
+            )
             args.append(args[-1] + self.h)
-            values.append(values[-1] + self.h * (k_1 + 2*k_1+2*k_3+k_4) / 6)
+            values.append(values[-1] + self.h * (k_1 + 2 * k_1 + 2 * k_3 + k_4) / 6)
         return args, values
